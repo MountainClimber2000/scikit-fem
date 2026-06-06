7ffcc2e3d890a86ed009e4a888e8cfb162ad33c8
KyleWinkler <winkler.kyle@outlook.com>
2026-06-07 07:19:00 +1000

    optimise visualize()

--- docs/examples/ex01.py
+++ docs/examples/ex01.py
@@ -1,38 +1,30 @@
 from skfem import *
 from skfem.helpers import dot, grad
-
 # # enable additional mesh validity checks, sacrificing performance
 # import logging
 # logging.basicConfig(format='%(levelname)s %(asctime)s %(name)s %(message)s')
 # logging.getLogger('skfem').setLevel(logging.DEBUG)
-
 # create the mesh
 m = MeshTri().refined(6)
 # or, with your own points and cells:
 # m = MeshTri(points, cells)
 # or, load from file
 # m = MeshTri.load("mesh.msh")
-
 e = ElementTriP1()
 basis = Basis(m, e)
-
-
 @BilinearForm
+
 def laplace(u, v, _):
     return dot(grad(u), grad(v))
-
 
 @LinearForm
 def rhs(v, _):
     return 1.0 * v
 
-
 A = laplace.assemble(basis)
 b = rhs.assemble(basis)
-
 # enforce Dirichlet boundary conditions
 A, b = enforce(A, b, D=m.boundary_nodes())
-
 # solve -- can be anything that takes a sparse matrix and a right-hand side
 x = solve(A, b)
 
